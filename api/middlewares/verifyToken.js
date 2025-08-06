4ba2ce712f80c7e563ad72f85f189d961920517a
shiwank pathak <shiwankpathak12@gmail.com>
2025-08-07 04:19:11 +0530

    verify token function

--- api/middlewares/verifyToken.js
+++ api/middlewares/verifyToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import createError from "../utils/error.js";
-
+//Verify token function
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(createError(401,"You are not authenticated!"))
